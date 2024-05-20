2744016686aee030ef242df996d9863da489f680
Vlado <pandzic.vlado@gmail.com>
2024-05-20 11:00:20 +0200

    fixed .net aspire project

--- src/Procoding.ApplicationTracker.AppHost/Program.cs
+++ src/Procoding.ApplicationTracker.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Procoding_ApplicationTracker_Api>("procoding-applicationtracker-api");
-
 builder.AddProject<Projects.Procoding_ApplicationTracker_Web_Root>("procoding-applicationtracker-web-root");
+
+builder.AddProject<Projects.Procoding_ApplicationTracker_Api>("procoding-applicationtracker-api");
 
 builder.Build().Run();
