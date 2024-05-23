e7a7596d14ede00d3b9d2f96f9b235b1cf26b091
Vlado <pandzic.vlado@gmail.com>
2024-05-23 21:13:14 +0200

    removed unnecessary dependencies from Web project

--- src/Procoding.ApplicationTracker.Web/Program.cs
+++ src/Procoding.ApplicationTracker.Web/Program.cs
@@ -1,18 +1,3 @@
-﻿//using Procoding.ApplicationTracker.Application;
-
-using Procoding.ApplicationTracker.Web.ViewModels;
+﻿
 
 namespace Procoding.ApplicationTracker.Web.Root;
-
-internal class Program
-{
-    static async Task Main(string[] args)
-    {
-        var app = new AppAdapter(args, typeof(Program), x =>
-        {
-            //x.AddTransient<IHandler, Handler>();
-        });
-
-        await app.StartAsync();
-    }
-}
